5b014adf3d97b598038b3981db63fb8e7a32f796
Raihan <rhnchdri@gmail.com>
2023-10-03 12:41:33 +0600

    js add

---
+++ script.js
@@ -0,0 +1,7 @@
+let clickLink = document.querySelector('.container');
+let regLink = document.querySelector('.sign-link');
+let logLink = document.querySelector('.login-link');
+
+regLink.addEventListener('click', () => {
+    clickLink.classList.add('active');
+});
